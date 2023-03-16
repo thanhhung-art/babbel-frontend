5099e181bfe4d9f2b7a4f5d00088a1645eba5a47
thanh-hung <thanhhungdeveloper@gmail.com>
2023-03-16 13:01:55 +0700

    edit file main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,4 @@ import ReactDOM from 'react-dom/client'
 import Container from './AppContainer'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render( <Container /> )
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render( <Container /> )
